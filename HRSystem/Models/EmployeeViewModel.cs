24af2b6209dded6be08b573e14b9534134bd42b8
Levon Hovhannisyan <lehovhan@microsoft.com>
2022-08-25 20:34:15 +0400

    Edit added

--- HRSystem/Models/EmployeeViewModel.cs
+++ HRSystem/Models/EmployeeViewModel.cs
@@ -4,8 +4,11 @@ namespace HRSystem.UI.Models
 {
     public class EmployeeViewModel
     {
+        public int id { get; set; }
+
         [Display(Name ="Անուն")]
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Email { get; set; }
     }
 }
